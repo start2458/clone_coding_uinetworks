9b69021f16e0a0c7c59ffea21ff8c68d91a2fdf8
JHP._.05 <start2458@gmail.com>
2022-12-01 11:11:44 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -5,19 +5,25 @@ $(".lbtn").click(function(){
     let next = (current-1)%3;
 
         if(next < 3){
-            fadeDiv.eq(current+1).fadeOut();
-            fadeDiv.eq(next+1).fadeIn();
+            fadeDiv.eq(current+1).fadeOut(700);
+            fadeDiv.eq(next+1).fadeIn(700);
             current = next;
         }else{
-            fadeDiv.eq(current).fadeOut();
-            fadeDiv.eq(next).fadeIn();
+            fadeDiv.eq(current).fadeOut(700);
+            fadeDiv.eq(next).fadeIn(700);
             current = next;
         }
 });
 $(".rbtn").click(function(){
         let next = (current+1)%3;
     
-        fadeDiv.eq(current).fadeOut();
-        fadeDiv.eq(next).fadeIn();
+        fadeDiv.eq(current).fadeOut(700);
+        fadeDiv.eq(next).fadeIn(700);
         current = next;
+});
+$(".hmenu").mouseover(function(){
+    $(".submenu").show();
+});
+$(".hmenu").mouseleave(function(){
+    $(".submenu").hide();
 });
